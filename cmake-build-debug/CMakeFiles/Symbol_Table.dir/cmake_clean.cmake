fcac8449a4bfa37cfd3fd3f68667be2cb7a3a5f0
dulanovic <vidan134@live.com>
2019-01-08 06:05:40 +0100

    Initial project upload

---
+++ cmake-build-debug/CMakeFiles/Symbol_Table.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Symbol_Table.dir/src/test.c.obj"
+  "CMakeFiles/Symbol_Table.dir/src/symtable.c.obj"
+  "Symbol_Table.pdb"
+  "Symbol_Table.exe"
+  "Symbol_Table.exe.manifest"
+  "libSymbol_Table.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Symbol_Table.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/symtable.c
@@ -0,0 +1,25 @@
+
+#include <stdlib.h>
+#include "symtable.h"
+
+struct SymbolTable {
+    //
+};
+
+SymTable smtb_new(void) {}
+void smtb_free(SymTable symTable) {}
+size_t smtb_getLength(SymTable symTable) {}
+int smtb_put(SymTable symTable, const char *key, const char *value) {}
+void *smtb_replace(SymTable symTable, const char *key, const char *value) {}
+int smtb_contains(SymTable symTable, const char *key) {}
+void *smtb_get(SymTable symTable, const char *key) {}
+void *smtb_remove(SymTable symTable, const char *key) {}
+void smtb_map(SymTable symTable, void (*func)(const char *key, void *value, void *extra), const void *extra) {}
+
+int main(int argc, char **argv) {
+
+    //
+
+    return(EXIT_SUCCESS);
+
+}
